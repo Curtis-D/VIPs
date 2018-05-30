a9ff782570e1908d56d76f5e43c027b56c4b460c
Curtis D <curtis.dransfield@hotmail.co.uk>
2018-05-30 21:10:25 +0100

    New feature that puts VIPs at the top of your Direct Messages

--- VIPs.plugin.js
+++ VIPs.plugin.js
@@ -6,11 +6,29 @@ var VIPs = function() {
     return class VIPs {
         getName() { return "VIPs"; }
         getDescription() { return "Adds an extra section to the friends list where you can add your most important contacts on Discord (Bots included). Add users by right clicking their name, opening their profile and then clicking on the star."; }
-        getVersion() { return "1.0.3"; }
+        getVersion() { return "1.1.0"; }
         getAuthor() { return "Green"; }
         getUpdateLink() { return "https://raw.githubusercontent.com/Greentwilight/VIPs/master/VIPs.plugin.js"; }
-
         load() {}
+
+        constructor() {
+            this.defaultSettings = {GroupDMs: true, VIPPinDMs: true};
+            this.settings = this.defaultSettings;
+        }
+        
+        generateSettings(panel) {
+            PluginUtilities.loadSettings(this.getName(), this.defaultSettings);
+            new PluginSettings.ControlGroup("VIP Settings", () => {PluginUtilities.saveSettings(this.getName(), this.settings);}, {shown: true}).appendTo(panel).append(
+                new PluginSettings.Checkbox("Pin VIPs to DMs", "", this.settings.VIPPinDMs, (checked) => {this.settings.VIPPinDMs = checked;}),
+                new PluginSettings.Checkbox("Include Group DMs", "", this.settings.GroupDMs, (checked) => {this.settings.GroupDMs = checked;})
+            );
+        }
+
+        getSettingsPanel() {
+            var panel = $("<form>").addClass("form").css("width", "100%");
+            if (this.initialized) this.generateSettings(panel);
+		    return panel[0];
+        }
 
         start() {
             var libraryScript = document.getElementById('zeresLibraryScript');
@@ -26,21 +44,66 @@ var VIPs = function() {
 
 
         initialize() {
-            this.initialized = true;
+            let self = this;
+            self.initialized = true;           
             PluginUtilities.checkForUpdate(this.getName(), this.getVersion(), this.getUpdateLink());
             const Friends = InternalUtilities.WebpackModules.findByDisplayName("Friends");
+            const DirectMessages = InternalUtilities.WebpackModules.findByDisplayName("LazyScroller");
+            let VIPIndex = -1;
+            let DMIndex = -1;
+
+            Patcher.before(this.getName(), DirectMessages.prototype, "render", function(thisObject, args, returnValue){
+                PluginUtilities.loadSettings(self.getName(), self.defaultSettings);
+                let data = PluginUtilities.loadData("VIPs", "VIPs", "");
+                if(self.settings.VIPPinDMs){
+                    for (var index = 0; index < thisObject.props.children.length; index++){
+                        let child = thisObject.props.children[index];
+                        if(child && data.ids && data.ids.length > 0){
+                            if(child.key == "Direct Messages" && thisObject.props.children[index-1].key != "VIPs"){
+                                if(VIPIndex == index || VIPIndex == -1){
+                                    let VIPSection = DiscordModules.React.cloneElement(child);
+                                    VIPIndex = index;
+                                    VIPSection.key = "VIPs";
+                                    VIPSection.props.children = "VIPs";
+                                    thisObject.props.children.splice(index, 0, VIPSection);
+                                    DMIndex = index + 1;
+                                }
+                            }
+                            if(child.props.channel){
+                                for (var idCounter = 0; idCounter < data.ids.length; idCounter++){
+                                    let id = data.ids[idCounter];
+                                    if(index > DMIndex && child.props.channel.recipients[0] == id){
+                                        if(self.settings.GroupDMs){
+                                            thisObject.props.children.splice(DMIndex, 0, thisObject.props.children.splice(index, 1)[0]);
+                                            DMIndex += 1;
+                                        } else {
+                                            if(child.props.channel.type == 1){
+                                                thisObject.props.children.splice(DMIndex, 0, thisObject.props.children.splice(index, 1)[0]);
+                                                DMIndex += 1;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
 
             Patcher.after(this.getName(), Friends.prototype, "render", function(thisObject, args, returnValue) {
                 let user;
                 let data = PluginUtilities.loadData("VIPs", "VIPs", "");
                 if(data.ids){
                     if(data.ids.length > 0){
-                        data.ids.forEach((id) => {
+                        for (var idCounter = 0; idCounter < data.ids.length; idCounter++){
+                            let id = data.ids[idCounter];
                             if((thisObject.state.rows._rows[0]) && (user = DiscordModules.UserStore.getUser(id))){
                                 let mutualGuilds = [];
-                                Object.values(DiscordModules.GuildStore.getGuilds()).forEach((guild) => {
+                                for (var guildCounter = 0; guildCounter < Object.values(DiscordModules.GuildStore.getGuilds()).length; guildCounter++){
+                                    let guild = Object.values(DiscordModules.GuildStore.getGuilds())[guildCounter];
                                     if(DiscordModules.GuildMemberStore.isMember(guild.id, id)){ mutualGuilds.push(guild); }
-                                })
+                                }
                                 let objectRow = new (thisObject.state.rows._rows[0].constructor)({
                                     activity: DiscordModules.UserStatusStore.getActivity(id),
                                     key: id,
@@ -53,21 +116,23 @@ var VIPs = function() {
                                 });
                                 let found = thisObject.state.rows._rows.some((row) => row.key == objectRow.key && row.type == objectRow.type);
                                 if(!found){ thisObject.state.rows._rows.push(objectRow); }
-                                thisObject.state.rows._rows.forEach((row) => {
+                                for (var rowCounter = 0; rowCounter < thisObject.state.rows._rows.length; rowCounter++){
+                                    let row = thisObject.state.rows._rows[rowCounter];
                                     if(!(data.ids.some((id) => (row.type == 99 && row.key == id)) || (row.type != 99))){
                                         let index = thisObject.state.rows._rows.indexOf(row);
                                         if (index > -1) { thisObject.state.rows._rows.splice(index, 1); }
                                     }
-                                });
+                                };
                             }
-                        });
+                        };
                     } else {
-                        thisObject.state.rows._rows.forEach((row) => {
+                        for (var rowCounter = 0; rowCounter < thisObject.state.rows._rows.length; rowCounter++){
+                            let row = thisObject.state.rows._rows[rowCounter];
                             if(row.type == 99){
                                 let index = thisObject.state.rows._rows.indexOf(row);
                                 if (index > -1) { thisObject.state.rows._rows.splice(index, 1); }
                             }
-                        });
+                        };
                     }
                 }
                 
@@ -77,9 +142,10 @@ var VIPs = function() {
                 sections[sections.length-1].key = "VIP";
    
                 let VIPs = [];
-                thisObject.state.rows._rows.forEach((row) => {
+                for (var rowCounter = 0; rowCounter < thisObject.state.rows._rows.length; rowCounter++){
+                    let row = thisObject.state.rows._rows[rowCounter];
                     if(row.type == 99){ VIPs.push(row); }
-                });
+                };
 
                 try{
                     if(thisObject.state.section == "VIP"){
@@ -109,7 +175,8 @@ var VIPs = function() {
             Patcher.instead(this.getName(), Friends.prototype, "componentDidUpdate", function(thisObject) {
                 let vipRowNumber = 0;
                 if(thisObject.state.section == "VIP"){
-                    thisObject.state.rows._rows.forEach((row) => {
+                    for (var rowCounter = 0; rowCounter < thisObject.state.rows._rows.length; rowCounter++){
+                        let row = thisObject.state.rows._rows[rowCounter];
                         if(row.type == 99){
                             let additionalActions = document.querySelectorAll(".friends-column-actions-visible")[vipRowNumber];
                             let wrapper = document.createElement('div');
@@ -148,7 +215,7 @@ var VIPs = function() {
                             }
                             vipRowNumber++;
                         }
-                    })
+                    }
                 }
             });
 
@@ -159,7 +226,7 @@ var VIPs = function() {
 
        observer(e) {
             if(e.addedNodes.length && e.addedNodes[0].classList && e.addedNodes[0].classList.contains("modal-1UGdnR")){                     
-                let popout = document.querySelector(".noWrap-3jynv6.root-SR8cQa");
+                let popout = document.querySelector(".inner-1JeGVc").childNodes[0];
                 let actions = document.querySelector(".additionalActionsIcon-1FoUlE");
                 if(popout && actions){
                     let data = PluginUtilities.loadData("VIPs", "VIPs", "");
